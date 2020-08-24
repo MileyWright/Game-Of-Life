0c22cffb45a8480b9d446ab80418a1bdb3db63ec
Miley Wright <dishaliekwright@yahoo.com>
2020-08-24 16:12:56 -0400

    set useDarkMode hook

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -3,12 +3,14 @@ import {Route} from 'react-router-dom';
 import './App.css';
 import Grid from './Components/Grid/Grid';
 import MainPage from './Components/MainPage/MainPage';
+import Options from './Components/MainPage/options';
 
 function App() {
   return (
     <div className="App">
       <Route exact path='/' component={MainPage} />
       <Route exact path='/play' component={Grid} />
+      <Route exact path='/options' component={Options} />
     </div>
   );
 }
---
+++ game-of-life/src/Components/MainPage/options.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './MainPage.css';
+
+const Options = () => {
+
+    return (
+        <div className='mainContainer'>
+            <h1 className='title'>Game of Life</h1>
+            <div className='links'>
+                    <button>
+                        
+                    </button>
+            </div>
+        </div>
+    )
+}
+
+export default Options;
